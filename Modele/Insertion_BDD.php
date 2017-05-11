068baf3d4c7ad68ccb765807b211ba45ba4ae3a5
JeromeLEKIEFFRE <jerome.lekieffre@isep.fr>
2017-05-11 11:03:47 +0200

    Création de fonctions PHP de vérification

--- Modele/Insertion_BDD.php
+++ Modele/Insertion_BDD.php
@@ -1,3 +1,3 @@
 <?php
-
+require("connexion_db.php");
 ?>
--- Modele/Verification.php
+++ Modele/Verification.php
@@ -1,4 +1,28 @@
 <?php
 require("connexion_db.php");
 
+
+function idUtilisateur($db,$username){
+//Renvoie l'ID de l'utilisateur ayant le nom d'utilisateur donné
+//(Utilisable pour savoir si un nom d'utilisateur éxiste déjà)
+    $sql = 'SELECT idUtilisateur FROM utilisateurs WHERE NomUtilisateur = "'.$username.'"';
+    $reponse = $db->query($sql);
+    return $reponse;
+}
+
+
+function idMail($db,$userMail){
+//Renvoie l'ID de l'utilisateur ayant le mail donné
+    $sql = 'SELECT idUtilisateur FROM utilisateurs WHERE Mail = "'.$userMail.'"';
+    $reponse = $db->query($sql);
+    return $reponse;
+}
+
+function idNum($db,$userNum){
+//Renvoie l'ID de l'utilisateur ayant le numéro de téléphone donné
+    $sql = 'SELECT idUtilisateur FROM utilisateurs WHERE Numero = "'.$userNum.'" OR Mobile = "'.$userNum.'"';
+    $reponse = $db->query($sql);
+    return reponse;
+}
+
 ?>
