d5601900096789f5ddd54da7f085c2227d137426
Jimmy <wangjimmy25@gmail.com>
2017-05-22 00:19:01 +0200

    mise a jour page inscription

--- Controleur/Formulaire.php
+++ Controleur/Formulaire.php
@@ -3,10 +3,10 @@
 include ("../Modele/Verification.php");
 include ("../Modele/connexion_db.php");
 include ("../Modele/Insertion_BDD.php");
-$email = 'test@exemple.com';
+$email = "test@exemple.com";
 if ($_POST["valider"]
     AND $_POST["Nom_utilisateur"]!=NULL
-    AND idUtilisateur($db, $_POST["Nom_utilisateur"])==NULL
+    AND idUtilisateur($db, $_POST["Nom_utilisateur"])==NULL  //Bizarrement idUtilisateur($db, $_POST["Nom_utilisateur"]) vaut toujours autre chose que NULL
     AND $_POST["Mot_de_passe"]!=NULL
     AND $_POST["Confirmation_mot_de_passe"]!=NULL
     AND $_POST["Mot_de_passe"]==$_POST["Confirmation_mot_de_passe"]
@@ -19,14 +19,14 @@ if ($_POST["valider"]
     AND $_POST["Numero_de_telephone"]!=NULL ){
     insertNewUser($db, $_POST["Nom_utilisateur"], $_POST["Mot_de_passe"], NULL, NULL, NULL, $_POST["Numero_de_telephone"], NULL);
     newIdUtilisateur($db); //sql est censé pouvoir faire l'incrémentation automatiquement
-    newHome($db, idUtilisateur($db,$_POST["Nom_utilisateur"]),$_POST["Ville"], NULL, $_POST["Numero_de_telephone"], NULL, NULL, $_POST["Code_Postal"]);
-    header("Location : compte_cree.html");
+    //newHome($db, idUtilisateur($db,$_POST["Nom_utilisateur"]),$_POST["Ville"], NULL, $_POST["Numero_de_telephone"], NULL, NULL, $_POST["Code_Postal"]);
+    header('Location: ../HTML/compte_cree.html');
     exit;
 }
 
 
 else {
-    echo "Tous les champs n'ont pas été correctement remplis:";
+    echo "Tous les champs n'ont pas été correctement remplis :";
     echo "<br>";
     echo "<br>";
 
@@ -36,7 +36,7 @@ else {
         echo "<br>";
     } else {
 
-        if (idUtilisateur($db, $_POST["Nom_utilisateur"]) != NULL) {
+        if (idUtilisateur($db, $_POST["Nom_utilisateur"]) == NULL) {
             echo "Ce nom d'utilisateur est déjà utilisé";
             echo "<br>";
             echo "<br>";
@@ -49,16 +49,21 @@ else {
         echo "<br>";
     }
 
-    if ($_POST["Confirmation_mot_de_passe"] == NULL) {
-        echo "Vous devez confirmer votre mot de passe";
-        echo "<br>";
-        echo "<br>";
-    }
+    else {
+        if ($_POST["Confirmation_mot_de_passe"] == NULL) {
+            echo "Vous devez confirmer votre mot de passe";
+            echo "<br>";
+            echo "<br>";
+        }
 
-    if ($_POST["Mot_de_passe"] != $_POST["Confirmation_mot_de_passe"]) {
-        echo "Vous n'avez pas rentré le même mot de passe";
-        echo "<br>";
-        echo "<br>";
+        else {
+
+            if ($_POST["Mot_de_passe"] != $_POST["Confirmation_mot_de_passe"]) {
+                echo "Vous n'avez pas rentré le même mot de passe";
+                echo "<br>";
+                echo "<br>";
+            }
+        }
     }
 
     if ($_POST["Adresse"] == NULL) {
@@ -86,29 +91,23 @@ else {
     }
 
     else {
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            echo "Cette adresse mail est valide";
-            echo "<br>";
-            echo "<br>";
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)==true) {
+            if (idMail($db, $_POST["Email"]) == NULL) {
+                echo "Cette adresse mail est déjà utilisée";
+                echo "<br>";
+                echo "<br>";
+            }
         } else {
             echo "Cette adresse mail est invalide";
             echo "<br>";
             echo "<br>";
-            exit;
         }
+    }
 
-        if (idMail($db, $_POST["Email"]) != NULL) {
-            echo "Cette adresse mail est déjà utilisée";
-            echo "<br>";
-            echo "<br>";
-        }
-
-        if ($_POST["Numero_de_telephone"] == NULL) {
-            echo "Vous devez entrer un numéro de téléphone";
-            echo "<br>";
-            echo "<br>";
-        }
-
+    if ($_POST["Numero_de_telephone"] == NULL) {
+        echo "Vous devez entrer un numéro de téléphone";
+        echo "<br>";
+        echo "<br>";
     }
 }
 
