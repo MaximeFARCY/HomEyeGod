be3f1a5b324e74f251296512481a5fbc0847196f
Jimmy <wangjimmy25@gmail.com>
2017-05-21 22:20:19 +0200

    Formulaire.php (bientôt terminé)

--- Controleur/Formulaire.php
+++ Controleur/Formulaire.php
@@ -1,49 +1,49 @@
 <?php
 // Contrôleur pour le formulaire inscription
-include ("Modele/Verification.php");
-include ("Modele/connexion_db.php");
-include ("Modele/Insertion_BDD.php");
+include ("../Modele/Verification.php");
+include ("../Modele/connexion_db.php");
+include ("../Modele/Insertion_BDD.php");
 $email = 'test@exemple.com';
 if ($_POST["valider"]
-    AND $_POST["Nom d'utilisateur"]!=NULL
-    AND idUtilisateur($db, $_POST["Nom d'utilisateur"])==NULL
-    AND $_POST["Mot de passe"]!=NULL
-    AND $_POST["Confirmation de mot de passe"]!=NULL
-    AND $_POST["Mot de passe"]==$_POST["Confirmation de mot de passe"]
+    AND $_POST["Nom_utilisateur"]!=NULL
+    AND idUtilisateur($db, $_POST["Nom_utilisateur"])==NULL
+    AND $_POST["Mot_de_passe"]!=NULL
+    AND $_POST["Confirmation_mot_de_passe"]!=NULL
+    AND $_POST["Mot_de_passe"]==$_POST["Confirmation_mot_de_passe"]
     AND $_POST["Adresse"]!=NULL
-    AND $_POST["Code Postal"]!=NULL
+    AND $_POST["Code_Postal"]!=NULL
     AND $_POST["Ville"]!=NULL
     AND $_POST["Email"]!= NULL
     AND filter_var($email, FILTER_VALIDATE_EMAIL)
     AND idMail($db, $_POST["Email"])==NULL
-    AND $_POST["Numéro de téléphone"]!=NULL ){
-    insertNewUser($db, $_POST["Nom d'utilisateur"], $_POST["Mot de passe"], NULL, NULL, NULL, $_POST["Numéro de téléphone"], NULL);
+    AND $_POST["Numero_de_telephone"]!=NULL ){
+    insertNewUser($db, $_POST["Nom_utilisateur"], $_POST["Mot_de_passe"], NULL, NULL, NULL, $_POST["Numero_de_telephone"], NULL);
     newIdUtilisateur($db); //sql est censé pouvoir faire l'incrémentation automatiquement
-    newHome($db, idUtilisateur($db,$_POST["Nom d'utilisateur"]),$_POST["Ville"], NULL, $_POST["Numéro de téléphone"], NULL, NULL, $_POST["Code Postal"]);
-    header("Location : la page compte crée");
+    newHome($db, idUtilisateur($db,$_POST["Nom_utilisateur"]),$_POST["Ville"], NULL, $_POST["Numero_de_telephone"], NULL, NULL, $_POST["Code_Postal"]);
+    header("Location : compte_cree.html");
     exit;
 }
 
 
 else {
 
-    if ($_POST["Nom d'utilisateur"] == NULL) {
+    if ($_POST["Nom_utilisateur"] == NULL) {
         echo "Vous devez entrer un nom d'utilisateur";
     }
 
-    if (idUtilisateur($db, $_POST["Nom d'utilisateur"])!= NULL){
+    if (idUtilisateur($db, $_POST["Nom_utilisateur"])!= NULL){
         echo "Ce nom d'utilisateur est déjà utilisé";
     }
 
-    if ($_POST["Mot de passe"] == NULL) {
+    if ($_POST["Mot_de_passe"] == NULL) {
         echo "Vous devez entrer un mot de passe";
     }
 
-    if ($_POST["Confirmation de mot de passe"] == NULL) {
+    if ($_POST["Confirmation_mot_de_passe"] == NULL) {
         echo "Vous devez confirmer votre mot de passe";
     }
 
-    if ($_POST["Mot de passe"] != $_POST["Confirmation de mot de passe"]) {
+    if ($_POST["Mot_de_passe"] != $_POST["Confirmation_mot_de_passe"]) {
         echo "Vous n'avez pas rentré le même mot de passe";
     }
 
@@ -51,7 +51,7 @@ else {
         echo "Vous devez entrer une adresse";
     }
 
-    if ($_POST["Code Postal"] == NULL) {
+    if ($_POST["Code_Postal"] == NULL) {
         echo "Vous devez entrer un code Postal";
     }
 
@@ -74,16 +74,10 @@ else {
         echo "Cette adresse mail est déjà utilisée";
     }
 
-    if ($_POST["Numéro de téléphone"] == NULL) {
+    if ($_POST["Numero_de_telephone"] == NULL) {
         echo "Vous devez entrer un numéro de téléphone";
     }
 
 }
 
 ?>
-/**
- * Created by PhpStorm.
- * User: Jimmy
- * Date: 11/05/2017
- * Time: 10:16
- */
